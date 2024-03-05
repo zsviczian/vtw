1131bebdec21a6c4fc3a83eb4a965d188091ec10
zsviczian <viczian.zsolt@gmail.com>
2024-03-05 22:11:44 +0100

    toggle font change

--- scripts/font.js
+++ scripts/font.js
@@ -1,5 +1,4 @@
 document.addEventListener('DOMContentLoaded', () => {
-  // Function to set a cookie
   function setCookie(cookieName, cookieValue, expirationDays) {
       var d = new Date();
       d.setTime(d.getTime() + (expirationDays * 24 * 60 * 60 * 1000));
@@ -7,7 +6,6 @@ document.addEventListener('DOMContentLoaded', () => {
       document.cookie = cookieName + "=" + cookieValue + ";" + expires + ";path=/";
   }
 
-  // Function to get a cookie value
   function getCookie(cookieName) {
       var name = cookieName + "=";
       var decodedCookie = decodeURIComponent(document.cookie);
@@ -24,30 +22,42 @@ document.addEventListener('DOMContentLoaded', () => {
       return "";
   }
 
-  // Function to toggle font family and update cookie
-  function toggleFontFamily() {
-      const currentFont = getCookie("fontFamily");
-      const newFont = currentFont === "default" ? "virgil" : "default"; 
-      setCookie("fontFamily", newFont, 30);  
-      document.body.classList.toggle("virgil");
+  const toggleButton = document.createElement("button");
+
+  const setButtonFont = (font) => {
+    if(font === "virgil") {
+      toggleButton.classList.remove("virgil");
+      toggleButton.classList.add("default");
+    } else {
+      toggleButton.classList.remove("default");
+      toggleButton.classList.add("virgil");
+    }
   }
 
-  // Create a button to toggle font family
-  const toggleButton = document.createElement("button");
+  function toggleFontFamily() {
+      const currentFont = getCookie("fontFamily");
+      const newFont = (currentFont === "default") ? "virgil" : "default"; 
+      setCookie("fontFamily", newFont, 30);  
+      document.body.classList.toggle("virgil");
+      setButtonFont(newFont);
+  }
+
+
   toggleButton.textContent = "Toggle Font";
   toggleButton.style.position = "fixed";
   toggleButton.style.top = "10px";
   toggleButton.style.right = "10px";
-  toggleButton.style.borderColor = "#006666"; // Set border color
-  toggleButton.className = "btn btn-outline-secondary"; // Bootstrap class
+  toggleButton.style.borderColor = "#006666";
+  toggleButton.className = "btn btn-outline-secondary";
   toggleButton.onclick = toggleFontFamily;
   document.body.appendChild(toggleButton);
 
-  // Check cookie on page load and apply font family if set
   const fontCookie = getCookie("fontFamily");
   if (fontCookie === "default") {
     document.body.classList.remove("virgil");
+    setButtonFont("default");
   } else {
     if(!document.body.classList.contains("virgil")) document.body.classList.add("virgil");
+    setButtonFont("virgil");
   }
 });
